e995cffb4be47342b5aa630e04468da7202e3ea0
drmcarvalho <furiano.k@gmail.com>
2026-02-05 14:42:01 -0300

    refac

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-API_KEY = 'AIzaSyBPkdN-Rsrc6FY5yO-ZDUcrtBQSmbFKx9Q'
+API_KEY = 'AIzaSyBPkdN-Rsrc6FY5yO-ZDUcrtBQSmbFKx9Q'  # This API key is mock
 AI_PROVIDER = 'Google'
 API_URL_GEMINI = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent'
 API_KEY_GEMINI_HEADER = 'x-goog-api-key'
