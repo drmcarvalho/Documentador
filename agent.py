b2002eb0355f0e5b546bd1d965adf2e49734ea86
drmcarvalho <furiano.k@gmail.com>
2026-02-05 13:46:55 -0300

    impl gemini tests

--- agent.py
+++ agent.py
@@ -6,6 +6,7 @@ from multiprocessing import Process
 import logging.config
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
+import constants
 
 
 logging_config = {
@@ -23,19 +24,21 @@ logging_config = {
     },
 }
 
-CONFIG_ERROR_MESSAGE = "Configuration error: The JSON structure is invalid (schema mismatch) or " \
-                       "the file path specified in 'path_file' was not found or "\
-                       "the path specified in 'output' was not found."
+CONFIG_ERROR_MESSAGE = (
+    "Configuration error: The JSON structure is invalid (schema mismatch) or "
+    "the file path specified in 'path_file' was not found or "
+    "the path specified in 'output' was not found."
+)
 CONFIG_ERROR_INVALID_MESSAGE = "The config.json file is invalid"
 
 logging.config.dictConfig(logging_config)
 logger = logging.getLogger(__name__)
 
 
-def make_request(url, data, headers, method="GET"):
+def make_request(url, payload, headers, method="GET"):
     try:
-        payload = json.dumps(data).encode('utf-8')
-        req = Request(url, data=payload, headers=headers, method=method)
+        data = json.dumps(payload).encode("utf-8")
+        req = Request(url, data=data, headers=headers, method=method)
         response = urlopen(req)
         return {response.read().decode(), response.code}
     except HTTPError as e:
@@ -46,6 +49,14 @@ def make_request(url, data, headers, method="GET"):
 class DocumentadorAgent:
     _processes = []
     _config_path = "config.json"
+    _prompt_cfg = (
+        "Please convert the following code into a Control Flow Graph using graphviz dot language syntax.\n"
+        "```\n"
+        "{code}\n"
+        "```\n"
+        "The output must be only the CFG diagram in DOT language.\n"
+    )
+    _prompt_class_diagram = ""
 
     def __init__(self):
         self._load_config()
@@ -109,7 +120,9 @@ class DocumentadorAgent:
                         logger.error(f"Could not update config:\n\t{e}")
                 time.sleep(5)
         except KeyboardInterrupt:
-            logger.info(f"{multiprocessing.current_process()} got CTRL-C at {self._watch_config.__name__ }")
+            logger.info(
+                f"{multiprocessing.current_process()} got CTRL-C at {self._watch_config.__name__ }"
+            )
 
     def _send_prompt(self):
         pass
@@ -126,9 +139,40 @@ class DocumentadorAgent:
                         if st.st_mtime > updatefile:
                             updatefile = st.st_mtime
                             logger.info(f"File changed: {file.name}")
-                time.sleep(3)
+                            with open(file.resolve(), "r") as f:
+                                data = f.read()
+                                if not data:
+                                    continue
+                                if "graph" in trackfile:
+                                    prompt = self._prompt_cfg.format(code=data)
+                                    content, code = make_request(
+                                        constants.API_URL_GEMINI,
+                                        {
+                                            "contents": [
+                                                {
+                                                    "parts": [
+                                                        {
+                                                            "text": prompt
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            constants.API_KEY_GEMINI_HEADER: constants.API_KEY,
+                                            "Content-Type": "application/json"
+                                        },
+                                        "POST",
+                                    )
+                                    if 200 <= code <= 299:
+                                        pass
+                                if "class" in trackfile:
+                                    pass  # TODO: generate class diagram
+                time.sleep(60)
         except KeyboardInterrupt:
-            logger.info(f"{multiprocessing.current_process()} got CTRL-C at {self._watch_files.__name__}")
+            logger.info(
+                f"{multiprocessing.current_process()} got CTRL-C at {self._watch_files.__name__}"
+            )
 
     def start(self):
         for target in (self._watch_config, self._watch_files):
--- documentador.py
+++ documentador.py
@@ -1,9 +1,20 @@
 from agent import DocumentadorAgent, make_request
+import pathlib
+
 
 if __name__ == '__main__':
     """docAgent = DocumentadorAgent()
     docAgent.start()"""
 
-    content, code = make_request("https://httpbin.org/json", None, {"accept": "application/json"})
+    """content, code = make_request("https://httpbin.org/json", None, {"accept": "application/json"})
     print(code)
-    print(content)
+    print(content)"""
+
+    """_prompt_cfg = "Please convert the following code into a Control Flow Graph using graphviz dot language syntax.\n" \
+                  "```\n" \
+                  "{code}\n" \
+                  "```\n"\
+                  "The output must be only the CFG diagram in DOT language.\n"
+    print(_prompt_cfg.format(code="Teste de codigo\nLinha 2"))"""
+    path = pathlib.Path('C:\\temp\\report2.csv')
+    print(path.resolve())
